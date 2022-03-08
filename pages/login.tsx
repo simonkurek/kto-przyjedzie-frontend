fe95cd3da570068fec32afbd637fb64964063712
Simon Kurek <simonkurek@protonmail.com>
2022-03-08 08:20:07 +0100

    main page, auth module (only ui)

---
+++ pages/login.tsx
@@ -0,0 +1,81 @@
+import { NextPage } from "next/types";
+
+const Login: NextPage = () => {
+  return (
+    <>
+      <div className="bg-white ">
+        <div className="flex justify-center h-screen">
+          <div className="flex max-w-md pl-6 pr-6 mt-16 mx-auto lg:w-2/6">
+            <div className="flex-1">
+              <div className="text-center">
+                <h2 className="text-6xl font-bold text-blue-500 text-center">
+                  KtoPrzyjedzie
+                </h2>
+
+                <p className="text-3xl font-medium text-center mt-7 text-gray-700 p-5">
+                  Zaloguj się!
+                </p>
+              </div>
+
+              <div className="mt-8">
+                <form>
+                  <div>
+                    <label className="block mb-2 text-lg text-gray-600 ">
+                      Email
+                    </label>
+                    <input
+                      type="email"
+                      name="email"
+                      id="email"
+                      placeholder="example@example.com"
+                      className="block w-full h-14 px-4 py-2 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-md     focus:border-blue-400  focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40"
+                    />
+                  </div>
+
+                  <div className="mt-6">
+                    <div className="flex justify-between mb-2">
+                      <label className="text-lg text-gray-600">Hasło</label>
+                      <a
+                        href="forgot-password"
+                        className="text-sm text-gray-400 focus:text-blue-500 hover:text-blue-500 hover:underline"
+                      >
+                        Zapomniałeś hasła?
+                      </a>
+                    </div>
+
+                    <input
+                      type="password"
+                      name="password"
+                      id="password"
+                      placeholder="Hasło"
+                      className="block w-full h-14 px-4 py-2 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-md   focus:border-blue-400  focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40"
+                    />
+                  </div>
+
+                  <div className="mt-6">
+                    <button className="w-full text-2xl mt-8 py-4 px-4 tracking-wide text-white transition-colors duration-200 transform bg-blue-500 rounded-md hover:bg-blue-400">
+                      Zaloguj się!
+                    </button>
+                  </div>
+                </form>
+
+                <p className="mt-6 text-sm text-center text-gray-400">
+                  Nie masz konta?{" "}
+                  <a
+                    href="signup"
+                    className="text-blue-500 focus:outline-none focus:underline hover:underline"
+                  >
+                    Zarejestruj się!
+                  </a>
+                  .
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Login;
---
+++ pages/signup.tsx
@@ -0,0 +1,104 @@
+import React from "react";
+
+const SignUp = () => {
+  return (
+    <>
+      <div className="bg-white ">
+        <div className="flex justify-center">
+          <div className="flex max-w-md pl-6 pr-6 mt-16 mb-20 mx-auto lg:w-2/6">
+            <div className="flex-1">
+              <div className="text-center">
+                <h2 className="text-6xl font-bold text-blue-500 text-center">
+                  KtoPrzyjedzie
+                </h2>
+
+                <p className="text-3xl font-medium text-center mt-7 text-gray-700 p-5">
+                  Zarejestruj się!
+                </p>
+              </div>
+
+              <div className="mt-8">
+                <form>
+                  <div>
+                    <label className="block mb-2 text-lg text-gray-600 ">
+                      Email
+                    </label>
+                    <input
+                      type="email"
+                      name="email"
+                      id="email"
+                      placeholder="example@example.com"
+                      className="block w-full h-14 px-4 py-2 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-md     focus:border-blue-400  focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40"
+                    />
+                  </div>
+
+                  <div className="mt-8">
+                    <label className="block mb-2 text-lg text-gray-600 ">
+                      Imię
+                    </label>
+                    <input
+                      type="name"
+                      name="name"
+                      id="name"
+                      placeholder="Imię"
+                      className="block w-full h-14 px-4 py-2 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-md     focus:border-blue-400  focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40"
+                    />
+                  </div>
+
+                  <div className="mt-6">
+                    <div className="flex justify-between mb-2">
+                      <label className="text-lg text-gray-600">Hasło</label>
+                    </div>
+
+                    <input
+                      type="password"
+                      name="password"
+                      id="password"
+                      placeholder="Hasło"
+                      className="block w-full h-14 px-4 py-2 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-md   focus:border-blue-400  focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40"
+                    />
+                  </div>
+
+                  <div className="mt-6">
+                    <div className="flex justify-between mb-2">
+                      <label className="text-lg text-gray-600">
+                        Potiwedź Hasło
+                      </label>
+                    </div>
+
+                    <input
+                      type="password"
+                      name="password"
+                      id="password"
+                      placeholder="Potwiedź Hasło"
+                      className="block w-full h-14 px-4 py-2 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-md   focus:border-blue-400  focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40"
+                    />
+                  </div>
+
+                  <div className="mt-6">
+                    <button className="w-full text-2xl mt-8 py-4 px-4 tracking-wide text-white transition-colors duration-200 transform bg-blue-500 rounded-md hover:bg-blue-400">
+                      Zarejestruj się!
+                    </button>
+                  </div>
+                </form>
+
+                <p className="mt-6 text-sm text-center text-gray-400">
+                  Masz ju konto?{" "}
+                  <a
+                    href="login"
+                    className="text-blue-500 focus:outline-none focus:underline hover:underline"
+                  >
+                    Zaloguj się!
+                  </a>
+                  .
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default SignUp;
